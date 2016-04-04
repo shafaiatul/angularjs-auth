0f4d2badb0588923b6fc25df95c6c7deedcaba93
Shafaiatul Islam <shafaiatul@gmail.com>
2016-04-04 00:55:42 -0400

    Initialize app and Inject the dependencies

---
+++ README.md
@@ -0,0 +1,5 @@
+##Planning
+1) Sign up for Auth0
+2) Simple AngularJS FrontEnd
+3) Simple NodeJS Backend with Express
+4) Send the JWT as an authorization header 
---
+++ app.js
@@ -0,0 +1,6 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('authApp', ['auth0', 'angular-storage', 'angular-jwt', 'ngMaterial', 'ui.router']);
+})();
